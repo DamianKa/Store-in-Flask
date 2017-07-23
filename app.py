927d509ffe5e4d325171c4ceff2a90cf18852ae7
DamianKa <damcash@gmail.com>
2017-07-23 11:56:37 +0100

    First draft
    First draft of Store in Flask created

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+
+app = Flask(__name__)
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+            'name': 'My item',
+            'price': 9.99
+            }
+        ]
+    }
+]
+
+
+#POST /store  data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+#GET / store <string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+#GET /store
+@app.route('/store')
+def get_stores():
+    pass
+#POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+#GET / store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
+
+
+
+
+app.run(port=5000)
